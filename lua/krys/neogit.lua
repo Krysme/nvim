1ef459d4378727dedfe1d40ccbe223b20c5c5974
yangdonglin <donglin.yang@bkunyun.com>
2023-03-07 17:44:54 +0800

    save editmsg

--- lua/krys/neogit.lua
+++ lua/krys/neogit.lua
@@ -1,10 +1,10 @@
 local function double_control_c_to_save_quit()
-    vim.keymap.set({ 'n', 'i' }, '<C-c><C-c>', ':wq<CR>', { buffer = 0 })
+    vim.keymap.set({ 'n', 'i' }, '<C-c><C-c>', '<cmd>wq<CR>', { buffer = true })
 end
 
 local group = vim.api.nvim_create_augroup("lsp-hooks", { clear = true })
 vim.api.nvim_create_autocmd('BufEnter', {
     group = group,
-    pattern = "*/COMMIT_EDITMSG",
+    pattern = "*COMMIT_EDITMSG",
     callback = double_control_c_to_save_quit,
 })
