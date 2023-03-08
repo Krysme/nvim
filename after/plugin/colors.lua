c37834deae37b0509f65ee2fc1389505df26fdc3
yangdonglin <donglin.yang@bkunyun.com>
2023-03-08 11:23:57 +0800

    improved lsp experience

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,6 @@
-function color_my_pencils(color)
-	color = color or 'rose-pine'
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-	vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+local function set_color_scheme(color)
+    color = color or 'onedark'
+    vim.cmd.colorscheme(color)
 end
 
-color_my_pencils()
+set_color_scheme()
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,41 +1,32 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
-lsp.ensure_installed({ 'rust_analyzer' })
+lsp.ensure_installed({ 'rust_analyzer', 'clangd' })
 
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<Tab>'] = cmp.mapping.confirm({ select = true }),
 })
 
 
-lsp.configure('sumneko_lua', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
+require "fidget".setup {}
 
 lsp.setup_nvim_cmp({ mapping = cmp_mappings })
 
 local function lsp_autocmd(client, bufnr)
     local group = vim.api.nvim_create_augroup("lsp-hooks", { clear = true })
     if client.supports_method "textDocument/documentHighlight" then
-
         vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
             group = group,
             buffer = bufnr,
             callback =
-            function()
-                vim.lsp.buf.document_highlight()
-                --       vim.lsp.buf.hover()
-            end
+                function()
+                    vim.lsp.buf.document_highlight()
+                    --       vim.lsp.buf.hover()
+                end
         })
         vim.api.nvim_create_autocmd("CursorMoved", {
             group = group,
@@ -71,8 +62,58 @@ lsp.on_attach(function(client, bufnr)
 end)
 
 
-require('nvim-lightbulb').setup({ autocmd = { enabled = true } })
-
+require('nvim-lightbulb').setup({
+    -- LSP client names to ignore
+    -- Example: {"sumneko_lua", "null-ls"}
+    ignore = {},
+    sign = {
+        enabled = false,
+        -- Priority of the gutter sign
+        priority = 10,
+    },
+    float = {
+        enabled = false,
+        -- Text to show in the popup float
+        text = "💡",
+        -- Available keys for window options:
+        -- - height     of floating window
+        -- - width      of floating window
+        -- - wrap_at    character to wrap at for computing height
+        -- - max_width  maximal width of floating window
+        -- - max_height maximal height of floating window
+        -- - pad_left   number of columns to pad contents at left
+        -- - pad_right  number of columns to pad contents at right
+        -- - pad_top    number of lines to pad contents at top
+        -- - pad_bottom number of lines to pad contents at bottom
+        -- - offset_x   x-axis offset of the floating window
+        -- - offset_y   y-axis offset of the floating window
+        -- - anchor     corner of float to place at the cursor (NW, NE, SW, SE)
+        -- - winblend   transparency of the window (0-100)
+        win_opts = {},
+    },
+    virtual_text = {
+        enabled = true,
+        -- Text to show at virtual text
+        -- text = "💡",
+        text = "💡",
+        -- highlight mode to use for virtual text (replace, combine, blend), see :help nvim_buf_set_extmark() for reference
+        hl_mode = "replace",
+    },
+    status_text = {
+        enabled = false,
+        -- Text to provide when code actions are available
+        text = "💡",
+        -- Text to provide when no actions are available
+        text_unavailable = ""
+    },
+    autocmd = {
+        enabled = true,
+        -- see :help autocmd-pattern
+        pattern = { "*" },
+        -- see :help autocmd-events
+        events = { "CursorHold", "CursorHoldI" }
+    }
+})
 
 lsp.setup()
 
