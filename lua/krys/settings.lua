7e2b7366c35d004b2b8a2177805d0de720dd8007
Krys <332407470@qq.com>
2023-03-11 14:01:38 +0800

    font flexibility

--- lua/krys/settings.lua
+++ lua/krys/settings.lua
@@ -23,9 +23,16 @@ vim.opt.backup = false
 vim.opt.termguicolors = true
 
 
+local wayland_env = os.getenv("WAYLAND_DISPLAY")
+
+if wayland_env ~= nil then
+    vim.o.guifont = "Jetbrains Mono:h15"
+else
+    -- X11 with a smaller font
+    vim.o.guifont = "Jetbrains Mono:h10"
+end
 
 -- for NeoVide
-vim.o.guifont = "Jetbrains Mono:h10"
 vim.g.neovide_scale_factor = 1.0
 vim.opt.linespace = 0
 vim.g.neovide_cursor_animation_length = 0
