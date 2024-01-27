4b1ac3e97430322acd4a2919e856c6c11fd3142e
Krys <blackshop.s.bossss@gmail.com>
2024-01-27 21:13:14 +0800

    fix errors at startup

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require 'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "help", "rust" },
+  ensure_installed = { "c", "lua", "rust" },
   sync_install = false,
 
   auto_install = true,
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/yangdonglin/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/yangdonglin/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/yangdonglin/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/yangdonglin/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/yangdonglin/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/krys/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?.lua;/home/krys/.cache/nvim/packer_hererocks/2.1.1702233742/share/lua/5.1/?/init.lua;/home/krys/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?.lua;/home/krys/.cache/nvim/packer_hererocks/2.1.1702233742/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/krys/.cache/nvim/packer_hererocks/2.1.1702233742/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,171 +76,161 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["FTerm.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/FTerm.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/FTerm.nvim",
     url = "https://github.com/numToStr/FTerm.nvim"
   },
   LuaSnip = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-nvim-lua"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
     url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["dressing.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/dressing.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/dressing.nvim",
     url = "https://github.com/stevearc/dressing.nvim"
   },
   ["fidget.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/fidget.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/fidget.nvim",
     url = "https://github.com/j-hui/fidget.nvim"
   },
   ["friendly-snippets"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/friendly-snippets",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
   ["lsp-status.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/lsp-status.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/lsp-status.nvim",
     url = "https://github.com/nvim-lua/lsp-status.nvim"
   },
   ["lsp-zero.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
     url = "https://github.com/VonHeikemen/lsp-zero.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["matchparen.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/matchparen.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/matchparen.nvim",
     url = "https://github.com/monkoose/matchparen.nvim"
   },
   ["neodev.nvim"] = {
     config = { "\27LJ\2\n4\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\vneodev\frequire\0" },
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/neodev.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/neodev.nvim",
     url = "https://github.com/folke/neodev.nvim"
   },
   neogit = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/neogit",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/neogit",
     url = "https://github.com/TimUntersberger/neogit"
-  },
-  ["nvim-autopairs"] = {
-    config = { "\27LJ\2\n<\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
-    loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
-    url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lightbulb"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/nvim-lightbulb",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/nvim-lightbulb",
     url = "https://github.com/kosayoda/nvim-lightbulb"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["onedark.nvim"] = {
     config = { "\27LJ\2\n;\0\0\3\0\4\0\0066\0\0\0009\0\1\0009\0\2\0'\2\3\0B\0\2\1K\0\1\0\fonedark\16colorscheme\bcmd\bvim\0" },
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/onedark.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/onedark.nvim",
     url = "https://github.com/navarasu/onedark.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["rose-pine"] = {
     config = { "\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0" },
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/rose-pine",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/rose-pine",
     url = "https://github.com/rose-pine/neovim"
   },
   ["telescope-file-browser.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
     url = "https://github.com/nvim-telescope/telescope-file-browser.nvim"
   },
   ["telescope-lsp-handlers.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/telescope-lsp-handlers.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/telescope-lsp-handlers.nvim",
     url = "https://github.com/gbrlsnchs/telescope-lsp-handlers.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/yangdonglin/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/krys/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   }
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: nvim-autopairs
-time([[Config for nvim-autopairs]], true)
-try_loadstring("\27LJ\2\n<\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0", "config", "nvim-autopairs")
-time([[Config for nvim-autopairs]], false)
 -- Config for: onedark.nvim
 time([[Config for onedark.nvim]], true)
 try_loadstring("\27LJ\2\n;\0\0\3\0\4\0\0066\0\0\0009\0\1\0009\0\2\0'\2\3\0B\0\2\1K\0\1\0\fonedark\16colorscheme\bcmd\bvim\0", "config", "onedark.nvim")
